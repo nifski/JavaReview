4a5021a243e63fd5bdd22da3b5af041aea46367d
oluwanifemi odumosu <77917304+nifski@users.noreply.github.com>
2025-03-30 02:43:18 +0100

    Simple version of sink ship game

---
+++ 2025-03-28/SimpleDotComTester.java
@@ -0,0 +1,21 @@
+SimpleDotComTester {
+	public static void main(Strings[] args)
+	SimpleDotCom dot = new SimpleDotCom ();
+	
+	int [] location = ["1", "2", "3"];
+	String dot.setLocationCells(location);
+	
+	String userGuess = "2";
+	String dot.checkYourself(userGuess);
+	
+	String testResult = "failed";
+	if (result.equals("hit")) { 
+		testResult = "passed"
+	}
+	System.out.println(testResult)
+}
+
+
+
+
+		
