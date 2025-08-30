11c7e966df7c352027f7945b02bbafc0717b58b9
oluwanifemi odumosu <77917304+nifski@users.noreply.github.com>
2025-08-30 12:03:54 +0100

    Implement OAuth2 Security Features

---
+++ SpringOauth2demo/src/main/java/com/nifemi/SpringOauth2demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.nifemi.SpringOauth2demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String greet(){
+        return "Hello Welcome to Pharmium, Pharmium.com";
+    }
+}
